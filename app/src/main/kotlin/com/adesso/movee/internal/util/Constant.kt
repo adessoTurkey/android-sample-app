419a9198e4dfd0981d4324ef9cff78f2c7dc0d83
Ocak, Mehmet <Mehmet.Ocak@adesso.com.tr>
2020-03-13 14:11:21 +0300

    Move show date format constant to ShowUiModel

--- app/src/main/kotlin/com/adesso/movee/internal/util/Constant.kt
+++ app/src/main/kotlin/com/adesso/movee/internal/util/Constant.kt
@@ -4,10 +4,6 @@ import java.util.Locale as JavaLocale
 
 object Constant {
 
-    object Date {
-        const val DATE_FORMAT_SHOW = "dd.MM.yyyy"
-    }
-
     object Locale {
         val LOCALE_TURKISH = JavaLocale.forLanguageTag("tr-TR")
         val LOCALE_ENGLISH = JavaLocale.ENGLISH
