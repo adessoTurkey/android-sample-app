b58425dd38fd7c093fd02f74c4766b1594b21090
Ocak, Mehmet <Mehmet.Ocak@adesso.com.tr>
2020-03-17 14:19:16 +0300

    Fix JsonDataException on null date

--- app/src/main/kotlin/com/adesso/movee/internal/util/DateAdapter.kt
+++ app/src/main/kotlin/com/adesso/movee/internal/util/DateAdapter.kt
@@ -1,32 +1,26 @@
 package com.adesso.movee.internal.util
 
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.FromJson
-import com.squareup.moshi.JsonReader
 import com.squareup.moshi.ToJson
-import com.squareup.moshi.JsonWriter
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Date
 
-class DateAdapter : JsonAdapter<Date>() {
+class DateAdapter {
     private val dateFormat = SimpleDateFormat(DATE_FORMAT_SERVER, Constant.Locale.LOCALE_ENGLISH)
 
     @FromJson
-    override fun fromJson(reader: JsonReader): Date? {
+    fun fromJson(date: String): Date? {
         return try {
-            val dateAsString = reader.nextString()
-            dateFormat.parse(dateAsString)
+            dateFormat.parse(date)
         } catch (e: ParseException) {
             null
         }
     }
 
     @ToJson
-    override fun toJson(writer: JsonWriter, value: Date?) {
-        if (value != null) {
-            writer.value(value.toString())
-        }
+    fun toJson(date: Date): String {
+        return dateFormat.format(date)
     }
 
     companion object {
