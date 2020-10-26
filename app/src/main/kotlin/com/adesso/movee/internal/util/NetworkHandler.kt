6f42b9ec31ebbc80329f32b48cedbb20397606b6
Murat Tuzel <mmurattuzel@gmail.com>
2020-10-26 13:59:09 +0300

    Fix deprecated network connectivity check
    Fix lazy initialization of okHttpClient
    
    Change gradle wrapper -bin distribution with -all
    
    Remove unnecessary comments from gradle versions plugin config

--- app/src/main/kotlin/com/adesso/movee/internal/util/NetworkHandler.kt
+++ app/src/main/kotlin/com/adesso/movee/internal/util/NetworkHandler.kt
@@ -1,11 +1,11 @@
 package com.adesso.movee.internal.util
 
 import android.content.Context
-import com.adesso.movee.internal.extension.networkInfo
+import com.adesso.movee.internal.extension.isNetworkAvailable
 import javax.inject.Singleton
 
 @Singleton
 class NetworkHandler(private val context: Context) {
     val isConnected: Boolean
-        get() = context.networkInfo?.isConnected ?: false
+        get() = context.isNetworkAvailable()
 }
